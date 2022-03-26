fa2bdd2d8a93d3b3cddc1a80bcefa3dfed2a4957
Khusham <khusham_u_din@icloud.com>
2022-03-26 19:29:38 +0530

    added the welcome page and displayed the courses appropriately

--- signup_api/signup_api/settings.py
+++ signup_api/signup_api/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'projectdatabase',
         'USER': 'root',
-        'PASSWORD': 'Tahoor013@',
+        'PASSWORD': 'tahoor013@',
         'HOST':'localhost',
         'PORT':'3306',
     }
